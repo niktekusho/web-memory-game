6d4713ad900bb048b99c2f8e994213c3833b926f
Nicola <nicola.dalmaso@outlook.com>
2018-01-15 14:22:50 +0100

    Step 3: WORKING game!

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const clickedArray = [];
+let clickedArray = [];
 let interval;
 let started = false;
 let time = 0;
+let ready = true;
+let numCompleted = 0;
 
 function randomAnswer() {
   const answers = [1,1,2,2,3,3,4,4,5];
   // see definition of sort method: https://developer.mozilla.org/it/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
   answers.sort(item => .5 - Math.random());
   return answers;
+}
+
+function hide(cell) {
+  cell.style.background = "blue";
+  cell.innerHTML = "";
+  cell.clicked = false;
+}
+
+function complete(cell) {
+  cell.style.background = "purple";
+  cell.completed = true;
+  numCompleted+=1;
 }
 
 function reveal(cell) {
@@ -52,12 +66,50 @@ function setUp() {
     });
     
     cell.addEventListener("click", function() {
+      if (ready === false) {
+        return;
+      } 
       startTimer();
       if (this.completed === false && this.clicked === false) {
         // add cell to clicked Array
         clickedArray.push(this);
         // reveal the cell
         reveal(this);
+      }
+      
+      // try to match numbers
+      if (clickedArray.length === 2) {
+        if (clickedArray[0].value === clickedArray[1].value) {
+          complete(clickedArray[0]);
+          complete(clickedArray[1]);
+          
+          clickedArray = [];
+          if (numCompleted === 8) {
+            alert(`WON in ${time} seconds!`);
+            clearInterval(interval);
+          }
+        } else {
+          // block click events
+          ready = false;
+          
+          // turn grid border to red
+          document.getElementById("gridTable").style.border = "5px solid red";
+          
+          
+          // let's factor in some delay (half a second)
+          setTimeout(() => {
+            hide(clickedArray[0]);
+            hide(clickedArray[1]);
+            
+            clickedArray = [];
+            
+            // enable click events again
+            ready = true;
+            
+            // restore grid border
+            document.getElementById("gridTable").style.border = "5px solid black";
+          }, 500);
+        }
       }
     });
   }
